2b266610f8995200cdd5df8552d45b01c875c51d
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2023-12-27 15:19:19 -0800

    define relations

--- models/index.js
+++ models/index.js
@@ -4,13 +4,33 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Tags belongToMany Products (through ProductTag)
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
+
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
+});
+
 
 module.exports = {
   Product,
